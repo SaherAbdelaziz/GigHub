8b0915a23288a802905d2f5c08a26d9ba85cb9b9
saher sayed abdelaziz <SaherSayed@cis.asu.edu.eg>
2020-05-11 13:04:02 +0200

    Implement Notification logic

---
+++ GigHub/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace GigHub.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        public DateTime? OriginalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+
+        [Required]
+        public Gig Gig { get; set; }
+    }
+}
